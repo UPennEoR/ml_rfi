53a53d94da982254e7c503e76e1f9fa282730a59
Josh Kerrigan <joshua_kerrigan@brown.edu>
2018-06-26 08:31:10 -0700

    Needs to be cleaned up.

--- FCN.py
+++ FCN.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 #from pyuvdata import UVData
 from xrfi import xrfi_simple
 tf.logging.set_verbosity(tf.logging.INFO)
+from time import time
 
 def import_test_data(filename,bl_tup=(9,89),rescale=1.0):
     uvd = UVData()
@@ -16,7 +17,7 @@ def import_test_data(filename,bl_tup=(9,89),rescale=1.0):
     data*=rescale
     return data
 
-def fold(data,ch_fold,labels=False):
+def fold(data,ch_fold=16,labels=False):
     # We want to fold over in frequency
     # this will be done for both waterfalls and labels
     # data should be in (times,freqs) format
@@ -40,14 +41,20 @@ def transpose(X):
 
 def normalize(X):
     sh = np.shape(X)
-    LOGabsX = np.nan_to_num(np.log10(np.abs(X+np.random.rand(sh[0],sh[1])))).real
+    LOGabsX = np.nan_to_num(np.log10(np.abs(X+(1e-7)*np.random.rand(sh[0],sh[1])))).real
     return (LOGabsX-np.nanmean(LOGabsX))/np.nanmax(np.abs(LOGabsX))
 
-def fold2(data,ch_fold,labels=False):
+def foldl(data,ch_fold=16):
     sh = np.shape(data)
     _data = data.T.reshape(ch_fold,sh[1]/ch_fold,-1)
     _DATA = np.array(map(transpose,_data))
-    DATA = _DATA#np.stack((np.array(map(normalize,_DATA)),np.angle(_DATA)),axis=-1)
+    return _DATA
+
+def fold2(data,ch_fold=16):
+    sh = np.shape(data)
+    _data = data.T.reshape(ch_fold,sh[1]/ch_fold,-1)
+    _DATA = np.array(map(transpose,_data))
+    DATA = np.stack((np.array(map(normalize,_DATA)),np.angle(_DATA)),axis=-1)
     return DATA
 
 def unfold(data_fold,nchans):
@@ -57,6 +64,12 @@ def unfold(data_fold,nchans):
         data[:,i*dfreqs:(i+1)*dfreqs] = data_fold[i,:,:]
     return data
 
+def unfold2(data_fold,nchans=1024):
+    ch_fold,ntimes,dfreqs = np.shape(data_fold)
+    data_ = np.array(map(transpose,data_fold))
+    _data = data_.reshape(ch_fold*dfreqs,ntimes).T
+    return _data
+    
 def stacked_layer(input_layer,num_filter_layers,kt,kf,activation,stride,pool,bnorm=True):
     """
     Creates a 3x stacked layer of convolutional layers ###
@@ -215,7 +228,7 @@ def cnn(features,labels,mode):
 def main(args):
     # load data
     f1 = h5py.File('RealVisRFI_v3.h5','r') # Load in a real dataset 
-    f2 = h5py.File('SimVisRFI_15_120_v3.h5','r') # Load in simulated data
+    f2 = h5py.File('SimVisRFI_v2.h5','r') # Load in simulated data
 
     train = True
     evaluate = True
@@ -239,25 +252,33 @@ def main(args):
     else:
         rnd_ind = np.random.randint(0,f1_r)
         samples = [rnd_ind]
-    for i in samples:        
-        print i
-        if i == samples[0]:
-            f_real = fold(f1['data'][i,:,:],16)
-            f_real_labels = fold(f1['flag'][i,:,:],16,labels=True)
-        else:
-            f_real = np.vstack((f_real,fold(f1['data'][i,:,:],16)))
-            f_real_labels = np.vstack((f_real_labels,fold(f1['flag'][i,:,:],16,labels=True)))
+    time0 = time()
+    f_real = np.array(map(fold2,f1['data'][:f1_r,:,:])).reshape(-1,60,64,2)
+    f_real_labels = np.array(map(foldl,f1['flag'][:f1_r,:,:])).reshape(-1,60,64)
+    print 'Training dataset loaded.'
+    
+    #for i in samples:        
+    #    print i
+    #    if i == samples[0]:
+    #        f_real = fold2(f1['data'][i,:,:],16)
+    #        f_real_labels = fold2(f1['flag'][i,:,:],16,labels=True)
+    #    else:
+    #        f_real = np.vstack((f_real,fold2(f1['data'][i,:,:],16)))
+    #        f_real_labels = np.vstack((f_real_labels,fold2(f1['flag'][i,:,:],16,labels=True)))
 
     # Cut up sim dataset and labels
     if train:
-        for i in range(f2_s):
-            print i
-            if i ==0:
-                f_sim = fold(f2['data'][i,:,:],16)
-                f_sim_labels = fold(f2['flag'][i,:,:],16,labels=True)
-            else:
-                f_sim = np.vstack((f_sim,fold(f2['data'][i,:,:],16)))
-                f_sim_labels = np.vstack((f_sim_labels,fold(f2['flag'][i,:,:],16,labels=True)))
+        f_sim = np.array(map(fold2,f2['data'][:f2_s,:,:])).reshape(-1,60,64,2)
+        f_sim_labels = np.array(map(foldl,f2['flag'][:f2_s,:,:])).reshape(-1,60,64)
+        print 'Simulated training dataset loaded.'
+#        for i in range(f2_s):
+#            print i
+#            if i ==0:
+#                f_sim = fold2(f2['data'][i,:,:],16)
+#                f_sim_labels = fold2(f2['flag'][i,:,:],16,labels=True)
+#            else:
+#                f_sim = np.vstack((f_sim,fold2(f2['data'][i,:,:],16)))
+#                f_sim_labels = np.vstack((f_sim_labels,fold2(f2['flag'][i,:,:],16,labels=True)))
 
     real_sh = np.shape(f_real)
 
@@ -268,26 +289,26 @@ def main(args):
 
     # Format training dataset
     if train|evaluate:
-        train_data = np.asarray(np.vstack((f_sim,f_real[:real_sh[0]/2,:,:,:])),dtype=np.float32)
-        train_labels = np.asarray(np.vstack((f_sim_labels,f_real_labels[:real_sh[0]/2,:,:])),dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])
+        train_data = np.asarray(f_sim,dtype=np.float32)#np.asarray(np.vstack((f_sim,f_real[:real_sh[0]/2,:,:,:])),dtype=np.float32)
+        train_labels = np.asarray(f_sim_labels,dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])#np.asarray(np.vstack((f_sim_labels,f_real_labels[:real_sh[0]/2,:,:])),dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])
 
         train0 = np.shape(train_data)[0]
         eval1 = np.shape(eval_data)[0]
-        steps = 10*train0
+        steps = train0
 
     # Format a single test dataset
     if test:
-        test_data = np.asarray(fold(f1['data'][rnd_ind,:,:],16), dtype=np.float32) # Random real visibility for testing
-        test_labels = np.asarray(fold(f1['flag'][rnd_ind,:,:],16,labels=True), dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])
+        test_data = np.asarray(fold2(f1['data'][rnd_ind,:,:],16), dtype=np.float32) # Random real visibility for testing
+        test_labels = np.asarray(foldl(f1['flag'][rnd_ind,:,:],16), dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])
 
     # create Estimator
-    rfiCNN = tf.estimator.Estimator(model_fn=cnn,model_dir='./checkpoint_Patch4/')
+    rfiCNN = tf.estimator.Estimator(model_fn=cnn,model_dir='./checkpoint_Patch4_SimTrain/')
 
     if train:
         train_input_fn = tf.estimator.inputs.numpy_input_fn(
             x={"x":train_data},
             y=train_labels,
-            batch_size=10,
+            batch_size=16,
             num_epochs=1000,
             shuffle=True,
         )
@@ -297,35 +318,48 @@ def main(args):
             x={"x":eval_data},
             y=eval_labels,
             num_epochs=100,
-            shuffle=False)	
+            shuffle=False
+        )	
 
     if test:
         test_input_fn = tf.estimator.inputs.numpy_input_fn(
-            x={"x":test_data},
+            x={"x":test_data}, ##### like 900 waterfalls!!!
             shuffle=False
         )
     
     if train:
         rfiCNN.train(input_fn=train_input_fn, steps=steps)
+
     if evaluate:
         eval_results = rfiCNN.evaluate(input_fn=eval_input_fn)
+        try:
+            output = open('eval_results.txt','w')
+            output.write(eval_results)
+        except:
+            print 'No eval results saved.'
         print(eval_results)
+
     if test:
         rfiPredict = rfiCNN.predict(input_fn=test_input_fn)
 
     # Predict on the test dataset where labels are hidden
+    print 'Prediction dataset is size: ',np.shape(train_data)[0]
+    obs_flags = np.zeros((16,60,64))#np.shape(train_data)[0],60,64))
     for i,predicts in enumerate(rfiPredict):
-        print np.shape(i),np.shape(predicts['probabilities'])
-        if i == 0:
-            cnn_flags = predicts['classes'].reshape(1,60,64)
-        else:
-            cnn_flags = np.vstack((cnn_flags,predicts['classes'].reshape(1,60,64)))
+        print i#np.shape(i),np.shape(predicts['probabilities'])
+        obs_flags[i,:,:] = predicts['classes'].reshape(60,64)
+#        if i == 0:
+#            cnn_flags = predicts['classes'].reshape(1,60,64)
+#        else:
+#            cnn_flags = np.vstack((cnn_flags,predicts['classes'].reshape(1,60,64)))
 
+    obs_flags = obs_flags.reshape(16,60,64)
+    cnn_flags = unfold2(obs_flags)
+    print time() - time0
     print 'Shape of CNN flags: ',np.shape(cnn_flags)
     print 'Shape of Test flags: ',np.shape(test_labels)
-    cnn_flags = unfold(cnn_flags,1024)
-    test_labels = unfold(test_labels.reshape(-1,60,64),1024)
-
+    #cnn_flags = unfold2(cnn_flags,1024)
+    test_labels = unfold2(test_labels.reshape(-1,60,64),1024)
     plt.subplot(411)
     plt.imshow(cnn_flags,aspect='auto')
     plt.title('Predicted Flags')
