ce87704e73b75b5bd115913eacfdf55b85d75ace
Josh Kerrigan <joshua_kerrigan@brown.edu>
2018-07-26 13:46:45 -0700

    Identical network without using an estimator API.

---
+++ FCN_2.py
@@ -0,0 +1,150 @@
+from __future__ import division, print_function, absolute_import
+import matplotlib
+matplotlib.use("AGG")
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from glob import glob
+import helper_functions as hf
+from time import time
+import os
+
+# Run on a single GPU
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+
+# Training Params                                                                                                                                  
+num_steps = 10000
+batch_size = 16
+pad_size = 68
+model_dir = np.sort(glob("./FCN/model_*"))
+try:
+    model = model_dir[-1].split('/')[-1].split('.')[0]+'.ckpt'
+    start_step = int(model.split('_')[1].split('.ckpt')[0])
+    print(model)
+except:
+    start_step = 0
+    print('Starting training at step %i' % start_step)
+mode = 'train'
+
+# Generator Network                                                                                                                               
+# Input: Noise, Output: Image                                                                                                                     
+
+def FCN(x, reuse=None):
+    with tf.variable_scope('FCN', reuse=reuse):
+        kt = 3
+        kf = 3
+        activation = tf.nn.relu
+        sh = x.get_shape().as_list()
+        input_layer = tf.cast(tf.reshape(x,[-1,sh[1],sh[2],2]),dtype=tf.float32)
+
+        # Convolution / Downsampling layers
+        slayer1 = hf.stacked_layer(input_layer,16,kt,kf,activation,[2,2],[2,2],bnorm=True)
+        s1sh = slayer1.get_shape().as_list()
+        slayer2 = hf.stacked_layer(slayer1,32,kt,kf,activation,[2,2],[2,2],bnorm=True)
+        slayer3 = hf.stacked_layer(slayer2,64,kt,kf,activation,[2,2],[2,2],bnorm=True)
+        slayer4 = hf.stacked_layer(slayer3,128,kt,kf,activation,[2,2],[2,2],bnorm=True)
+        s4sh = slayer4.get_shape().as_list()
+        slayer5 = tf.layers.dropout(hf.stacked_layer(slayer4,256,1,1,activation,[2,2],[2,2],bnorm=True),rate=0.5,training=True)
+        slayer6 = hf.stacked_layer(slayer5,512,1,1,activation,[2,2],[2,2],bnorm=True)
+
+        # Fully connected and convolutional layers
+        slayer7 = tf.layers.dropout(hf.stacked_layer(slayer6,2048,1,1,activation,[1,1],[1,1],bnorm=True),rate=0.5,training=True)
+        slayer8 = hf.stacked_layer(slayer7,2048,1,1,activation,[1,1],[1,1],bnorm=True)
+
+        # Transpose convolution layers
+        upsamp1 = tf.layers.conv2d_transpose(slayer8,filters=1,kernel_size=[s4sh[1],s4sh[1]],activation=activation)
+        upsamp1 = tf.add(upsamp1,tf.reshape(tf.reduce_max(slayer4,axis=-1),[-1,s4sh[1],s4sh[1],1]))
+        upsamp1 = tf.contrib.layers.batch_norm(upsamp1,scale=True)
+        upsamp2 = tf.layers.conv2d_transpose(upsamp1,filters=1,kernel_size=[s4sh[1]+1,s4sh[1]+1],activation=activation)
+        upsamp2 = tf.contrib.layers.batch_norm(upsamp2,scale=True)
+        upsamp3 = tf.layers.conv2d_transpose(upsamp2,filters=1,kernel_size=[s1sh[1]-2*s4sh[1]+1,s1sh[1]-2*s4sh[1]+1],activation=activation)
+        upsamp3 = tf.add(upsamp3,tf.reshape(tf.reduce_max(slayer1,axis=-1),[-1,s1sh[1],s1sh[1],1]))
+        upsamp3 = tf.contrib.layers.batch_norm(upsamp3,scale=True)
+        upsamp4 = tf.layers.conv2d_transpose(upsamp3,filters=2,kernel_size=[int(sh[1]/2) + 1,int(sh[1]/2) + 1],activation=activation)
+        upsamp4 = tf.contrib.layers.batch_norm(upsamp4,scale=True)
+        final_conv = tf.reshape(upsamp4,[-1,sh[1]*sh[1],2])
+        
+    return final_conv
+
+# Build Networks                                                                                                                                  
+# Network Inputs                                                                                                                                  
+vis_input = tf.placeholder(tf.float32, shape=[None, pad_size, pad_size, 2]) #this is a waterfall amp/phs visibility                               
+
+# Build Generator Network                                                                                                                         
+RFI_guess = FCN(vis_input)
+bsl_RFI = tf.summary.image(name='Baseline Metrics',tensor=RFI_guess)
+
+summary = tf.summary.merge_all()
+
+RFI_targets = tf.placeholder(tf.int32, shape=[None, pad_size*pad_size])
+
+loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=RFI_guess, labels=RFI_targets)
+optimizer_gen = tf.train.AdamOptimizer(learning_rate=0.01)
+fcn_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='FCN')
+train_fcn = optimizer_gen.minimize(loss, var_list=fcn_vars)
+
+# Initialize the variables (i.e. assign their default value)                                                                                      
+init = tf.global_variables_initializer()
+# save variables                                                                                                                                  
+saver = tf.train.Saver()
+
+
+# Load dataset
+dset = hf.RFIDataset()
+dset.load(batch_size)
+
+with tf.Session() as sess:
+
+    summary_writer = tf.summary.FileWriter('./FCN/',sess.graph)
+    # Run the initializer                                                                                                                         
+    sess.run(init)
+    #check to see if model exists                                                                                                                 
+    if len(model_dir) > 0:
+        print('Model exists. Loading last save.')
+        saver.restore(sess, './FCN/'+model)
+        print('Model '+str(model) + ' loaded.')
+    else:
+        print('No Model Found.')
+    if mode == 'train':
+        for i in range(start_step, start_step+num_steps+1):
+            # Prepare Input Data                                                                                                                  
+            batch_x, batch_targets = dset.next_train()
+            # Training                                                                                                                           
+            feed_dict = {vis_input: batch_x, RFI_targets: batch_targets}
+            _,loss_ = sess.run([train_fcn,loss],feed_dict=feed_dict)
+            #summ1 = sess.run(rflagsdelay, feed_dict=feed_dict)
+            #summ2 = sess.run(gflagsdelay, feed_dict=feed_dict)
+            #summary_writer.add_summary(summ1,i)
+            #summary_writer.add_summary(summ2,i)
+            if i % 100 == 0 or i == 1:
+                print('Step %i: RFI Crushinator Loss: %.9f' % (i, np.mean(loss_)))
+            if i % 1000 == 0:
+                print('Saving model...')
+                summary_writer.flush()
+                save_path = saver.save(sess,'./FCN/model_%i.ckpt' % i)
+
+    elif mode == 'eval':
+        for i in range(1, num_steps+1):
+            batch_x, batch_targets = dset.next_eval()
+            feed_dict = {vis_input: batch_x, RFI_targets: batch_targets}
+            eval_class = sess.run(RFI_guess,feed_dict=feed_dict)
+            if i % 10 == 0:
+                if 'acc' not in globals():
+                    acc = 0.
+                acc = hf.batch_accuracy(batch_targets,tf.argmax(eval_class,axis=-1)).eval()
+                print('Batch accuracy %f' % acc)
+    else:
+        batch_x, batch_targets = dset.random_test()
+        time0 = time()
+        g = sess.run(RFI_guess, feed_dict={vis_input: batch_x})
+        print('Single baseline time: ',time() - time0)
+        
+        plt.subplot(211)
+        plt.imshow(batch_x[0,:,:,0],aspect='auto')
+        plt.colorbar()
+        plt.text(512,30,tf.argmax(g,axis=1).eval())
+        plt.subplot(212)
+        plt.imshow(batch_x[0,:,:,1],aspect='auto')
+        plt.colorbar()
+        plt.savefig('VisClassify.pdf')
+
---
+++ helper_functions.py
@@ -0,0 +1,189 @@
+import numpy as np
+import tensorflow as tf
+import h5py
+from time import time
+import os
+
+
+def transpose(X):
+    """
+    Transpose for use in the map functions.
+    """
+    return X.T
+
+def normalize(X):
+    """
+    Normalization for the log amplitude required in the folding process.
+    """
+    sh = np.shape(X)
+    LOGabsX = np.nan_to_num(np.log10(np.abs(X+(1e-5)*np.random.rand(sh[0],sh[1])))).real
+    return (LOGabsX-np.nanmean(LOGabsX))/np.nanstd(np.abs(LOGabsX))
+    
+def foldl(data,ch_fold=16):
+    """
+    Folding function for carving up a waterfall visibility flags for prediction in the FCN.
+    """
+    sh = np.shape(data)
+    _data = data.T.reshape(ch_fold,sh[1]/ch_fold,-1)
+    _DATA = np.array(map(transpose,_data))
+    _DATApad = np.array(map(pad,_DATA))
+    return _DATApad
+
+def pad(data,padding=2):
+    sh = np.shape(data)
+    t_pad = (sh[1] - sh[0])/2
+    data_pad = np.pad(data,pad_width=((t_pad+padding,t_pad+padding),(padding,padding)),mode='reflect')
+    return data_pad
+
+def unpad(data,diff=4,padding=2):
+    sh = np.shape(data)
+    t_unpad = sh[0]
+    # time axis isnt unpadding correctly
+    return data[padding/2+diff/2:,padding:][:-padding/2-diff/2,:-padding][padding/2:,:][:-padding/2,:]
+                      
+def fold(data,ch_fold=16):
+    """
+    Folding function for carving waterfall visibilities with additional normalized log 
+    and phase channels.
+    """
+    sh = np.shape(data)
+    _data = data.T.reshape(ch_fold,sh[1]/ch_fold,-1)
+    _DATA = np.array(map(transpose,_data))
+    _DATApad = np.array(map(pad,_DATA))
+    DATA = np.stack((np.array(map(normalize,_DATApad)),np.angle(_DATApad)),axis=-1)
+    return DATA
+
+def unfoldl(data_fold,nchans=1024):
+    """
+    Unfolding function for recombining the carved label (flag) frequency windows back into a complete 
+    waterfall visibility.
+    """
+    data_unpad = np.array(map(unpad,data_fold))
+    ch_fold,ntimes,dfreqs = np.shape(data_unpad)
+    data_ = np.array(map(transpose,data_unpad))
+    _data = data_.reshape(ch_fold*dfreqs,ntimes).T
+    return _data
+
+def stacked_layer(input_layer,num_filter_layers,kt,kf,activation,stride,pool,bnorm=True,name='None'):
+    """
+    Creates a 3x stacked layer of convolutional layers. Each layer uses the same kernel size.
+    Batch normalized output is default and recommended for faster convergence, although
+    not every may require it (???).
+    """
+    conva = tf.layers.conv2d(inputs=input_layer,
+                             filters=num_filter_layers,
+                             kernel_size=[kt,kf],
+                             padding="same",
+                             activation=activation)
+    
+    convb = tf.layers.conv2d(inputs=conva,
+                             filters=num_filter_layers,
+                             kernel_size=[kt,kf],
+                             padding="same",
+                             activation=activation)
+
+    convc = tf.layers.conv2d(inputs=convb,
+                             filters=num_filter_layers,
+                             kernel_size=[kt,kf],
+                             padding="same",
+                             activation=activation)
+    if bnorm:
+    	bnorm_conv = tf.contrib.layers.batch_norm(convc,scale=True)
+    else:
+    	bnorm_conv = convc
+
+    pool = tf.layers.max_pooling2d(inputs=bnorm_conv,
+                                    pool_size=pool,
+                                    strides=stride)
+    return pool
+
+def batch_accuracy(labels,predictions):
+    labels = tf.cast(labels,dtype=tf.int64)
+    predictions = tf.cast(predictions,dtype=tf.int64)
+    correct = tf.reduce_sum(tf.cast(tf.equal(tf.add(labels,predictions),2),dtype=tf.int64))
+    total = tf.reduce_sum(labels)
+    return tf.divide(correct,total)
+
+def delay_transform(data,flags):
+    sh = data.get_shape().as_list()
+    data = tf.reshape(data,[-1,sh[1],sh[1],1])
+    flags = tf.cast(tf.reshape(flags,[-1,sh[1],sh[1],1]),dtype=tf.complex64)
+    #flags_ = tf.cast(tf.logical_not(tf.cast(flags,dtype=tf.bool)),dtype=tf.complex64)
+    data_ = tf.transpose(tf.multiply(data,flags),perm=[2,1,0,3])
+    DATA_ = tf.abs(tf.fft2d(data_))
+    DATA = tf.transpose(DATA_,perm=[2,1,0,3])
+    return DATA
+
+
+#def load_data():
+
+class RFIDataset():
+    def __init__(self):
+        print('Welcome to the HERA RFI training and evaluation dataset.')
+
+    def load(self,batch_size):
+        # load data
+        self.batch_size = batch_size
+        print('A batch size of %i has been set.' % self.batch_size)
+        f1 = h5py.File('RealVisRFI_v3.h5','r') # Load in a real dataset 
+        f2 = h5py.File('SimVisRFI_15_120_v3.h5','r') # Load in simulated data
+        #    f2 = h5py.File('RealVisRFI_v3.h5','r')
+
+        psize = 68 # Pixel pad size for individual carved bands
+        fcarve = 16 # Carving narrowband factor
+
+        # We want to augment our training dataset with the entirety of the simulated data
+        # but with only half of the real data. The remaining real data half will become
+        # the evaluation dataset
+    
+        f1_r = 900 #np.shape(f1['data'])[0]
+        f2_s = np.shape(f2['data'])[0]
+
+        print 'Size of real dataset: ',f1_r
+        print ''
+        # Cut up real dataset and labels
+        f1_r = 900#np.shape(f1['data'])[0] # Everything after 900 doesn't look good
+        samples = range(f1_r)
+        rnd_ind = np.random.randint(0,f1_r)
+        #else:
+        #    rnd_ind = 100#np.random.randint(0,f1_r)
+        #    samples = [rnd_ind]
+        time0 = time()
+    
+        f_real = np.array(map(fold,f1['data'][:f1_r,:,:])).reshape(-1,psize,psize,2)
+        f_real_labels = np.array(map(foldl,f1['flag'][:f1_r,:,:])).reshape(-1,psize,psize)
+        print 'Training dataset loaded.'
+        print 'Training dataset size: ',np.shape(f_real)
+        # Cut up sim dataset and labels
+        f_sim = np.array(map(fold,f2['data'][:f2_s,:,:])).reshape(-1,psize,psize,2)
+        f_sim_labels = np.array(map(foldl,f2['flag'][:f2_s,:,:])).reshape(-1,psize,psize)
+        print 'Simulated training dataset loaded.'
+        print 'Training dataset size: ',np.shape(f_sim)
+        real_sh = np.shape(f_real)
+
+        # Format evaluation dataset
+        self.eval_data = np.asarray(f_real[:,:,:,:],dtype=np.complex64)
+        self.eval_labels = np.asarray(f_real_labels[:,:,:],dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])
+        eval1 = np.shape(self.eval_data)[0]
+        
+        # Format training dataset
+        self.train_data = np.asarray(f_sim,dtype=np.complex64)#np.asarray(np.vstack((f_sim,f_real[:real_sh[0]/2,:,:,:])),dtype=np.float32)
+        self.train_labels = np.asarray(np.logical_not(f_sim_labels),dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])#np.asarray(np.vstack((f_sim_labels,f_real_labels[:real_sh[0]/2,:,:])),dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])
+
+        train0 = np.shape(self.train_data)[0]
+        self.test_data = np.asarray(fold(f1['data'][rnd_ind,:,:],fcarve), dtype=np.complex64) # Random real visibility for testing
+        self.test_labels = np.asarray(foldl(f1['flag'][rnd_ind,:,:],fcarve), dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])
+
+    def next_train(self):
+        self.train_data = np.roll(self.train_data,self.batch_size,axis=0)
+        self.train_labels = np.roll(self.train_labels,self.batch_size,axis=0)
+        return self.train_data[:self.batch_size,:,:,:],self.train_labels[:self.batch_size,:]
+
+    def next_eval(self):
+        self.eval_data = np.roll(self.eval_data,self.batch_size,axis=0)
+        self.eval_labels = np.roll(self.eval_labels,self.batch_size,axis=0)
+        return self.eval_data[:self.batch_size,:,:,:],self.eval_labels[:self.batch_size,:]
+
+    def random_test(self):
+        ind = np.random.randint(0,np.shape(self.eval_data))
+        return self.eval_data[ind,:,:,:],self.eval_labels[ind,:]
