9f85039611ba6276e20736faa7eef4b9d95c0fdb
Josh Kerrigan <joshua_kerrigan@brown.edu>
2018-08-17 06:28:21 -0700

    Swapped out batch norm functions. Cross val is on sim datset.

--- FCN_2.py
+++ FCN_2.py
@@ -13,26 +13,31 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 # Training Params                                                                                                                                 
-model_name = 'AmpPhsRELU'
-chtypes='AmpPhs'
-num_steps = 10000
-batch_size = 64
+model_name = 'AmpSimBSize8'
+chtypes='Amp'
+num_steps = 50001
+batch_size = 8
 pad_size = 68
-ch_input = 2
-mode = ''
-model_dir = np.sort(glob("./"+model_name+"/model_*"))
+ch_input = 1
+f_factor = 16
+mode = 'train'
+model_dir = glob("./"+model_name+"/model_*")
+
 try:
-    model = model_dir[-1].split('/')[-1].split('.')[0]+'.ckpt'
+    models2sort = [int(model_dir[i].split('/')[2].split('.')[0].split('_')[1]) for i in range(len(model_dir))]
+    model_ind = np.argmax(models2sort)
+    model = 'model_'+str(models2sort[model_ind])+'.ckpt'#model_dir[np.max(model_ind)].split('/')[2]+'.ckpt'
     start_step = int(model.split('_')[1].split('.ckpt')[0])
     print(model)
 except:
     start_step = 0
-    print('Starting training at step %i' % start_step)
+
+print('Starting training at step %i' % start_step)
 
 # Generator Network                                                                                                                               
 # Input: Noise, Output: Image                                                                                                                     
 
-def FCN(x, reuse=None):
+def FCN(x, reuse=None, mode=True):
     with tf.variable_scope('FCN', reuse=reuse):
         kt = 3
         kf = 3
@@ -40,41 +45,40 @@ def FCN(x, reuse=None):
         activation = tf.nn.leaky_relu
         sh = x.get_shape().as_list()
         input_layer = tf.cast(tf.reshape(x,[-1,sh[1],sh[2],sh[3]]),dtype=tf.float32)
-        
+        tf.summary.image('IP_Amp',tf.reshape(input_layer[0,:,:,0],[1,pad_size,pad_size,1]))
+#        tf.summary.image('IP_Phs',tf.reshape(input_layer[0,:,:,1],[1,pad_size,pad_size,1]))
         # Convolution / Downsampling layers
-        slayer1 = hf.stacked_layer(input_layer,s*16,kt,kf,activation,[2,2],[2,2],bnorm=True)
+        slayer1 = hf.stacked_layer(input_layer,s*16,kt,kf,activation,[2,2],[2,2],bnorm=True,mode=mode_bn)
         s1sh = slayer1.get_shape().as_list()
         print(s1sh)
-        tf.summary.image('Slayer_1',tf.reshape(tf.reduce_sum(slayer1[0,:,:,:],axis=-1),[1,34,34,1]))
-        slayer2 = hf.stacked_layer(slayer1,s*32,kt,kf,activation,[2,2],[2,2],bnorm=True)
-        slayer3 = hf.stacked_layer(slayer2,s*64,kt,kf,activation,[2,2],[2,2],bnorm=True)
+        tf.summary.image('S1',tf.reshape(tf.reduce_sum(slayer1[0,:,:,:],axis=-1),[1,int(pad_size/2),int(pad_size/2),1]))
+
+        slayer2 = hf.stacked_layer(slayer1,s*32,kt,kf,activation,[2,2],[2,2],bnorm=True,mode=mode_bn)
+        slayer3 = hf.stacked_layer(slayer2,s*64,kt,kf,activation,[2,2],[2,2],bnorm=True,mode=mode_bn)
         s3sh = slayer3.get_shape().as_list()
-        slayer4 = hf.stacked_layer(slayer3,s*128,kt,kf,activation,[2,2],[2,2],bnorm=True)
-        slayer5 = hf.stacked_layer(slayer4,s*512,kt,kf,activation,[2,2],[2,2],bnorm=True)
-        slayer6 = hf.stacked_layer(slayer5,s*1024,kt,kf,activation,[2,2],[2,2],bnorm=True)
+        slayer4 = hf.stacked_layer(slayer3,s*128,kt,kf,activation,[2,2],[2,2],bnorm=True,mode=mode_bn)
+        slayer5 = hf.stacked_layer(slayer4,s*512,kt,kf,activation,[2,2],[2,2],bnorm=True,mode=mode_bn)
+        slayer6 = hf.stacked_layer(slayer5,s*1024,kt,kf,activation,[2,2],[2,2],bnorm=True,mode=mode_bn)
         
         # Fully connected and convolutional layers
-        slayer7 = hf.stacked_layer(slayer6,s*2048,1,1,activation,[1,1],[1,1],bnorm=True,dropout=True)
-        slayer8 = hf.stacked_layer(slayer7,s*2048,1,1,activation,[1,1],[1,1],bnorm=True,dropout=False)
-        slayer8 = tf.contrib.layers.batch_norm(slayer8,scale=True)
+        slayer7 = hf.stacked_layer(slayer6,s*2048,1,1,activation,[1,1],[1,1],bnorm=True,dropout=True,mode=mode_bn)
+        slayer8 = hf.stacked_layer(slayer7,s*2048,1,1,activation,[1,1],[1,1],bnorm=True,dropout=True,mode=mode_bn)
         
         # Transpose convolution layers
         upsamp1 = tf.layers.conv2d_transpose(slayer8,filters=256,kernel_size=[s3sh[1],s3sh[1]],activation=activation)
         upsamp1 = tf.add(upsamp1,tf.reshape(tf.reduce_max(slayer3,axis=-1),[-1,s3sh[1],s3sh[1],1]))
+        upsamp1 = tf.layers.batch_normalization(upsamp1,scale=True,center=True,training=mode,fused=True)#tf.contrib.layers.batch_norm(upsamp1,scale=True)
 
-        upsamp1 = tf.contrib.layers.batch_norm(upsamp1,scale=True)
         upsamp2 = tf.layers.conv2d_transpose(upsamp1,filters=128,kernel_size=[s3sh[1]+1,s3sh[1]+1],activation=activation)
-
-        upsamp2 = tf.contrib.layers.batch_norm(upsamp2,scale=True)
+        upsamp2 = tf.layers.batch_normalization(upsamp2,scale=True,center=True,training=mode_bn,fused=True)#tf.contrib.layers.batch_norm(upsamp2,scale=True)
         upsamp3 = tf.layers.conv2d_transpose(upsamp2,filters=64,kernel_size=[s1sh[1]-2*s3sh[1]+1,s1sh[1]-2*s3sh[1]+1],activation=activation)
         upsamp3 = tf.add(upsamp3,tf.reshape(tf.reduce_max(slayer1,axis=-1),[-1,s1sh[1],s1sh[1],1]))
         upsh3 = upsamp3.get_shape().as_list()
-        tf.summary.image('Upsamp_3',tf.reshape(upsamp3[0,:,:,0],[1,34,34,1]))
-
-        upsamp3 = tf.contrib.layers.batch_norm(upsamp3,scale=True)
+        upsamp3 = tf.layers.batch_normalization(upsamp3,scale=True,center=True,training=mode_bn,fused=True)#tf.contrib.layers.batch_norm(upsamp3,scale=True)
+        tf.summary.image('Upsamp_3',tf.reshape(upsamp3[0,:,:,0],[1,int(pad_size/2),int(pad_size/2),1]))
         upsamp4 = tf.layers.conv2d_transpose(upsamp3,filters=2,kernel_size=[int(sh[1] - upsh3[1]) + 1,int(sh[1] - upsh3[1]) + 1],activation=activation)
-        upsamp4 = tf.contrib.layers.batch_norm(upsamp4,scale=True)
-
+        upsamp4 = tf.layers.batch_normalization(upsamp4,scale=True,center=True,training=mode_bn,fused=True)#tf.contrib.layers.batch_norm(upsamp4,scale=True)
+        tf.summary.image('Flag Guess',tf.reshape(upsamp4[0,:,:,1],[1,pad_size,pad_size,1]))
         final_conv = tf.reshape(upsamp4,[-1,sh[1]*sh[1],2])
         
     return final_conv
@@ -82,9 +86,9 @@ def FCN(x, reuse=None):
 # Build Networks                                                                                                                                  
 # Network Inputs                                                                                                                                  
 vis_input = tf.placeholder(tf.float32, shape=[None, pad_size, pad_size, ch_input]) #this is a waterfall amp/phs/comp visibility                               
-
+mode_bn = tf.placeholder(tf.bool)
 # Build Generator Network                                                                                                                         
-RFI_guess = FCN(vis_input)
+RFI_guess = FCN(vis_input,mode=mode_bn)
 #bsl_RFI = tf.summary.image(name='Baseline Metrics',tensor=RFI_guess)
 
 
@@ -101,7 +105,7 @@ batch_accuracy = hf.batch_accuracy(RFI_targets,argmax)
 f1 = 2.*precision[0]*recall[0]/(precision[0]+recall[0])
 f1 = tf.where(tf.is_nan(f1),tf.zeros_like(f1),f1)
 #loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=RFI_targets,logits=RFI_guess)) + 100.*(1-f1)
-loss = tf.losses.sparse_softmax_cross_entropy(labels=RFI_targets,logits=RFI_guess)#,weights=1.-batch_accuracy)
+loss = tf.losses.sparse_softmax_cross_entropy(labels=RFI_targets,logits=RFI_guess)#s,weights=1./batch_accuracy)
 #loss = tf.losses.softmax_cross_entropy(onehot_labels=RFI_targets,logits=argmax,weights=1.-f1)
 
 tf.summary.scalar('loss',loss)
@@ -124,7 +128,7 @@ saver = tf.train.Saver()
 
 # Load dataset
 dset = hf.RFIDataset()
-dset.load(batch_size,pad_size,hybrid=True,chtypes=chtypes)
+dset.load(batch_size,pad_size,hybrid=False,chtypes=chtypes,fold_factor=f_factor)
 
 with tf.Session() as sess:
 
@@ -135,7 +139,7 @@ with tf.Session() as sess:
     if len(model_dir) > 0:
         print('Model exists. Loading last save.')
         saver.restore(sess, './'+model_name+'/'+model)
-        print('Model '+str(model) + ' loaded.')
+        print('Model '+'./'+model_name+'/'+model + ' loaded.')
     else:
         print('No Model Found.')
     if mode == 'train':
@@ -146,7 +150,7 @@ with tf.Session() as sess:
             batch_x, batch_targets = dset.next_train()
             # Training                                                                                                                           
             feed_dict = {vis_input: batch_x, RFI_targets: batch_targets,
-                         learn_rate: lr}
+                         learn_rate: lr, mode_bn: True}
             _,loss_,s1,rec,pre,f1_,ba = sess.run([train_fcn,loss,summary,recall,precision,f1,batch_accuracy],feed_dict=feed_dict)
             #s1 = sess.run(s1image, feed_dict=feed_dict)
             #up3 = sess.run(up3image, feed_dict=feed_dict)
@@ -164,12 +168,14 @@ with tf.Session() as sess:
             if i % 1000 == 0 and i != 0:
                 print('Saving model...')
                 save_path = saver.save(sess,'./'+model_name+'/model_%i.ckpt' % i)
-                print('Learning rate decreased to %f.' % lr)
+#            if i % 10000 == 0:
+#                lr *= 0.1
+#                print('Learning rate decreased to %f.' % lr)
     elif mode == 'eval':
         eval_writer = tf.summary.FileWriter('./'+model_name+'_eval/',sess.graph)
         for i in range(1, num_steps+1):
             batch_x, batch_targets = dset.next_eval()
-            feed_dict = {vis_input: batch_x, RFI_targets: batch_targets}
+            feed_dict = {vis_input: batch_x, RFI_targets: batch_targets, mode_bn: True}
             eval_class,rec,pre,f1_,s1 = sess.run([RFI_guess,recall,precision,f1,summary],feed_dict=feed_dict)
             if i % 10 == 0:
                 if 'acc' not in globals():
@@ -186,27 +192,33 @@ with tf.Session() as sess:
         time0 = time()
         ind = 0
         batch_x, batch_targets = dset.random_test(16)
-        g = sess.run(RFI_guess, feed_dict={vis_input: batch_x})
-        acc = hf.accuracy(batch_targets[ind,:],tf.reshape(tf.argmax(g[ind,:],axis=-1),[1,-1]))
-        print('N=%i number of baselines time: ' % 16,time() - time0)
-        print('Accuracy compared to XRFI flags: %f'% acc)
+        g = sess.run(RFI_guess, feed_dict={vis_input: batch_x, mode_bn: True})
+        print('N=%i number of baselines time: ' % 1,time() - time0)
         for ind in range(16):
+            acc = hf.accuracy(batch_targets[ind,:],tf.reshape(tf.argmax(g[ind,:,:],axis=-1),[-1]).eval())
+            print('Accuracy compared to XRFI flags: %f'% acc)
             plt.figure()
-            plt.subplot(421)
-            plt.imshow(batch_x[ind,:,:,0],aspect='auto')
-            plt.title('Vis')
+            plt.subplot(321)
+            plt.imshow(hf.unpad(batch_x[ind,:,:,0]),aspect='auto')
+            plt.title('Amplitude')
             plt.colorbar()
-            plt.subplot(422)
-            plt.imshow(batch_targets[ind,:].reshape(pad_size,pad_size),aspect='auto')
+            plt.subplot(322)
+            plt.imshow(hf.unpad(batch_x[ind,:,:,0]),aspect='auto')
+            plt.title('Phase')
+            plt.colorbar()
+            
+            plt.subplot(323)
+            plt.imshow(hf.unpad(batch_targets[ind,:].reshape(pad_size,pad_size)),aspect='auto')
             plt.title('Targets')
             plt.colorbar()
-            plt.subplot(423)
-            plt.imshow(tf.reshape(tf.argmax(g[ind,:,:],axis=-1),[pad_size,pad_size]).eval(),aspect='auto')
+            plt.subplot(324)
+            plt.imshow(hf.unpad(tf.reshape(tf.argmax(g[ind,:,:],axis=-1),[pad_size,pad_size]).eval()),aspect='auto')
             plt.text(512,50,s=str(acc))
             plt.title('Argmax')
             plt.colorbar()
-            plt.subplot(424)
-            plt.imshow(tf.reshape(hf.hard_thresh(g[ind,:,:]),[pad_size,pad_size]).eval(),aspect='auto')
+            plt.subplot(313)
+            plt.imshow(hf.unpad(tf.reshape(hf.hard_thresh(g[ind,:,:]),[pad_size,pad_size]).eval()),aspect='auto')
             plt.title('Hard Threshold')
+            plt.tight_layout()
             plt.savefig(model_name+'_%i.pdf'%ind)
             plt.clf()
--- helper_functions.py
+++ helper_functions.py
@@ -4,6 +4,8 @@ import h5py
 from time import time
 import os
 
+np.random.seed(555)
+
 def transpose(X):
     """
     Transpose for use in the map functions.
@@ -15,16 +17,17 @@ def normalize(X):
     Normalization for the log amplitude required in the folding process.
     """
     sh = np.shape(X)
-    LOGabsX = np.nan_to_num(np.log10(np.abs(X+(1e-5)*np.random.rand(sh[0],sh[1])))).real
+    LOGabsX = np.nan_to_num(np.log10(np.abs(X+(1e-8)*np.random.rand(sh[0],sh[1])))).real
     return (LOGabsX-np.nanmean(LOGabsX))/np.nanstd(np.abs(LOGabsX))
 
 def normphs(X):
-    """                                                                                                                                                                                            
-    Normalization for the log amplitude required in the folding process.                                                                                                                           
+    """                                                                                                                                                                            
+    Normalization for the log amplitude required in the folding process.                                                                                                            
     """
     sh = np.shape(X)
     phsX = np.angle(X)
-    return (phsX - np.nanmean(phsX))/np.nanstd(np.abs(phsX))
+    phsX_ = np.mod(phsX,np.pi)
+    return (phsX_ - np.nanmean(phsX_))#/np.nanstd(np.abs(phsX_))
     
 def noisy_relu(x):
     try:
@@ -36,21 +39,24 @@ def noisy_relu(x):
 def leaky_relu(x,alpha=.8):
     return tf.nn.relu(x) - alpha * tf.nn.relu(-x)
 
-def foldl(data,ch_fold=16):
+def foldl(data,ch_fold=16,padding=2):
     """
     Folding function for carving up a waterfall visibility flags for prediction in the FCN.
     """
     sh = np.shape(data)
-    _data = data.T.reshape(ch_fold,sh[1]/ch_fold,-1)
+    _data = data.T.reshape(ch_fold,sh[1]/ch_fold,-1)#[2:14]
     _DATA = np.array(map(transpose,_data))
     _DATApad = np.array(map(pad,_DATA))
     return _DATApad
 
 def pad(data,padding=2):
     #for 68 pad with 2
+    #    100        18
     sh = np.shape(data)
     t_pad = (sh[1] - sh[0])/2
-    data_pad = np.pad(data,pad_width=((t_pad+padding,t_pad+padding),(padding,padding)),mode='symmetric')
+    data_pad = np.pad(data,pad_width=((t_pad+padding,t_pad+padding),(padding,padding)),mode='constant',constant_values=0)
+#    psh = np.shape(data_pad)
+#    data_pad = np.where(data_pad == 0.,np.max(data_pad)*np.random.rand(psh[0],psh[1]),data_pad)
     return data_pad
 
 def unpad(data,diff=4,padding=2):
@@ -59,19 +65,19 @@ def unpad(data,diff=4,padding=2):
     # time axis isnt unpadding correctly
     return data[padding/2+diff/2:,padding:][:-padding/2-diff/2,:-padding][padding/2:,:][:-padding/2,:]
                       
-def fold(data,ch_fold=16):
+def fold(data,ch_fold=16,padding=2):
     """
     Folding function for carving waterfall visibilities with additional normalized log 
     and phase channels.
     """
     sh = np.shape(data)
-    _data = data.T.reshape(ch_fold,sh[1]/ch_fold,-1)
+    _data = data.T.reshape(ch_fold,sh[1]/ch_fold,-1)#[2:14]
     _DATA = np.array(map(transpose,_data))
     _DATApad = np.array(map(pad,_DATA))
-    DATA = np.stack((np.array(map(normalize,_DATApad)),np.array(map(normphs,_DATApad)),_DATApad),axis=-1)
+    DATA = np.stack((np.array(map(normalize,_DATApad)),np.mod(np.array(map(normphs,_DATApad)),np.pi),np.mod(np.array(map(normphs,_DATApad)),np.pi)),axis=-1)
     return DATA
 
-def unfoldl(data_fold,nchans=1024):
+def unfoldl(data_fold,nchans=1024,ch_fold=16,padding=2):
     """
     Unfolding function for recombining the carved label (flag) frequency windows back into a complete 
     waterfall visibility.
@@ -82,7 +88,7 @@ def unfoldl(data_fold,nchans=1024):
     _data = data_.reshape(ch_fold*dfreqs,ntimes).T
     return _data
 
-def stacked_layer(input_layer,num_filter_layers,kt,kf,activation,stride,pool,bnorm=True,name='None',dropout=False):
+def stacked_layer(input_layer,num_filter_layers,kt,kf,activation,stride,pool,bnorm=True,name='None',dropout=False,mode=True):
     """
     Creates a 3x stacked layer of convolutional layers. Each layer uses the same kernel size.
     Batch normalized output is default and recommended for faster convergence, although
@@ -93,29 +99,33 @@ def stacked_layer(input_layer,num_filter_layers,kt,kf,activation,stride,pool,bno
                              kernel_size=[kt,kf],
                              padding="same",
                              activation=activation)
+                            
     if not dropout:
         convb = tf.layers.conv2d(inputs=conva,
                              filters=num_filter_layers,
                              kernel_size=[kt,kf],
                              padding="same",
                              activation=activation)
+                            
     else:
         convb = tf.layers.dropout(tf.layers.conv2d(inputs=conva,
                              filters=num_filter_layers,
                              kernel_size=[kt,kf],
                              padding="same",
-                             activation=activation),rate=.3)
+                                                   activation=activation),rate=.5)
+                             
         
     convc = tf.layers.conv2d(inputs=convb,
                              filters=num_filter_layers,
                              kernel_size=[1,1],
                              padding="same",
                              activation=activation)
+                             
     if bnorm:
-    	bnorm_conv = tf.contrib.layers.batch_norm(convc,scale=True)
+    	#bnorm_conv = tf.contrib.layers.batch_norm(convc,scale=True)
+        bnorm_conv = tf.layers.batch_normalization(convc,scale=True,center=True,training=mode,fused=True)
     else:
     	bnorm_conv = convc
-
     pool = tf.layers.max_pooling2d(inputs=bnorm_conv,
                                     pool_size=pool,
                                     strides=stride)
@@ -131,11 +141,12 @@ def batch_accuracy(labels,predictions):
 def accuracy(labels,predictions):
     correct = 1.*np.sum((labels+predictions)==2)
     total = 1.*np.sum(labels==1)
-    print(total)
+    print('correct',correct)
+    print('total',total)
     try:
         return correct/total
     except:
-        return 1.
+        return 0.
     
 def delay_transform(data,flags):
     sh = data.get_shape().as_list()
@@ -152,7 +163,7 @@ def delay_transform(data,flags):
     DATA = tf.transpose(DATA_,perm=[2,1,0,3])
     return tf.reduce_mean(tf.divide(DATA,DATA_noflags)[:,:,:,:])
 
-def hard_thresh(layer,thresh=1e-2):
+def hard_thresh(layer,thresh=1e-5):
     try:
         layer = layer[:,:,1]
     except:
@@ -165,29 +176,29 @@ class RFIDataset():
     def __init__(self):
         print('Welcome to the HERA RFI training and evaluation dataset.')
 
-    def load(self,batch_size,psize,hybrid=False,chtypes='AmpPhs'):
+    def load(self,batch_size,psize,hybrid=False,chtypes='AmpPhs',fold_factor=16):
         # load data
         self.chtypes = chtypes
         self.batch_size = batch_size
         print('A batch size of %i has been set.' % self.batch_size)
-        f1 = h5py.File('RealVisRFI_v3.h5','r') # Load in a real dataset 
-        f2 = h5py.File('SimVisRFI_15_120_v3.h5','r') # Load in simulated data
+        f1 = h5py.File('RealVisRFI_v3.h5') # 'RealVisRFI_v3.h5','r') # Load in a real dataset 
+        f2 = h5py.File('SimVis_v5.h5','r') # Load in simulated data
         #    f2 = h5py.File('RealVisRFI_v3.h5','r')
 
         self.psize = psize # Pixel pad size for individual carved bands
-        fcarve = 16 # Carving narrowband factor
+        #fold_factor = 16 # Carving narrowband factor
 
         # We want to augment our training dataset with the entirety of the simulated data
         # but with only half of the real data. The remaining real data half will become
         # the evaluation dataset
     
-        f1_r = 900 #np.shape(f1['data'])[0]
+        f1_r = np.shape(f1['data'])[0]
         f2_s = np.shape(f2['data'])[0]
 
         print 'Size of real dataset: ',f1_r
         print ''
         # Cut up real dataset and labels
-        f1_r = 900#np.shape(f1['data'])[0] # Everything after 900 doesn't look good
+        f1_r = np.shape(f1['data'])[0] # Everything after 900 doesn't look good
         samples = range(f1_r)
         rnd_ind = np.random.randint(0,f1_r)
         #else:
@@ -201,7 +212,7 @@ class RFIDataset():
             # Cut up sim dataset and labels
             f_sim = (np.array(map(fold,f2['data'][:f2_s,:,:]))[:,:,:,:,:2]).reshape(-1,self.psize,self.psize,2)
             f_sim_labels = np.array(map(foldl,f2['flag'][:f2_s,:,:])).reshape(-1,self.psize,self.psize)
-        elif chtypes == 'AmpPhsCmp' :
+        elif chtypes == 'AmpPhs2' :
             f_real = np.array(map(fold,f1['data'][:f1_r,:,:])).reshape(-1,self.psize,self.psize,3)
             f_real_labels = np.array(map(foldl,f1['flag'][:f1_r,:,:])).reshape(-1,self.psize,self.psize)
             # Cut up sim dataset and labels
@@ -252,31 +263,31 @@ class RFIDataset():
         else:
             # Format evaluation dataset
             sim_len = np.shape(f_sim)[0]
-            self.eval_data = np.asarray(f_sim[:sim_len/2,:,:,:],dtype=d_type)
-            self.eval_labels = np.asarray(f_sim_labels[:sim_len/2,:,:],dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])
+            self.eval_data = np.asarray(f_sim[int(sim_len/3):,:,:,:],dtype=d_type)
+            self.eval_labels = np.asarray(f_sim_labels[int(sim_len/3):,:,:],dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])
             eval1 = np.shape(self.eval_data)[0]
 
             # Format training dataset
             #self.train_data = np.asarray(f_real[:700,:,:,:],dtype=np.float64)
             #self.train_labels = np.asarray(f_real_labels[:700,:,:],dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])
-            self.train_data = np.asarray(f_sim[sim_len/2:,:,:,:],dtype=d_type)#np.asarray(np.vstack((f_sim,f_real[:real_sh[0]/2,:,:,:])),dtype=np.float32)
-            self.train_labels = np.asarray(f_sim_labels[sim_len/2:,:,:],dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])#np.asarray(np.vstack((f_sim_labels,f_real_labels[:real_sh[0]/2,:,:])),dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])
+            self.train_data = np.asarray(f_sim[:int(1-sim_len/3),:,:,:],dtype=d_type)#np.asarray(np.vstack((f_sim,f_real[:real_sh[0]/2,:,:,:])),dtype=np.float32)
+            self.train_labels = np.asarray(f_sim_labels[:(1-sim_len/3),:,:],dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])#np.asarray(np.vstack((f_sim_labels,f_real_labels[:real_sh[0]/2,:,:])),dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])
 
             train0 = np.shape(self.train_data)[0]
-            self.test_data = np.asarray(fold(f1['data'][rnd_ind,:,:],fcarve), dtype=d_type) # Random real visibility for testing
-            self.test_labels = np.asarray(foldl(f1['flag'][rnd_ind,:,:],fcarve), dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])
+            self.test_data = np.asarray(fold(f1['data'][rnd_ind,:,:]), dtype=d_type) # Random real visibility for testing
+            self.test_labels = np.asarray(foldl(f1['flag'][rnd_ind,:,:]), dtype=np.int32).reshape(-1,real_sh[1]*real_sh[2])
             self.eval_len = np.shape(self.eval_data)[0]
             self.train_len = np.shape(self.train_data)[0]
 
     def next_train(self):
-        self.train_data = np.roll(self.train_data,self.batch_size,axis=0)
-        self.train_labels = np.roll(self.train_labels,self.batch_size,axis=0)
+#        self.train_data = np.roll(self.train_data,self.batch_size,axis=0)
+#        self.train_labels = np.roll(self.train_labels,self.batch_size,axis=0)
         rand_batch = np.random.randint(0,self.train_len,size=self.batch_size)
         return self.train_data[rand_batch,:,:,:],self.train_labels[rand_batch,:]
 
     def next_eval(self):
-        self.eval_data = np.roll(self.eval_data,self.batch_size,axis=0)
-        self.eval_labels = np.roll(self.eval_labels,self.batch_size,axis=0)
+#        self.eval_data = np.roll(self.eval_data,self.batch_size,axis=0)
+#        self.eval_labels = np.roll(self.eval_labels,self.batch_size,axis=0)
         rand_batch = np.random.randint(0,self.eval_len,size=self.batch_size)
         return self.eval_data[rand_batch,:,:,:],self.eval_labels[rand_batch,:]
 
